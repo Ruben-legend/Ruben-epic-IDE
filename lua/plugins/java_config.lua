3b17a942f02b340c1aff280a96afea02238999ff
ruben-epic <rubenalexander644@outlook.es>
2023-11-05 20:54:04 -0400

    java config v1

--- lua/plugins/java_config.lua
+++ lua/plugins/java_config.lua
@@ -1,13 +1,20 @@
 return {
   "mfussenegger/nvim-jdtls",
+  dependencies = {
+    "mason-org/mason-registry"
+  },
   event = "BufRead",
   config = function()
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.ts = 4
+    vim.opt_local.expandtab = true
+    
     local config = {
-      cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/jdtls") },
-      root_dir = vim.fs.dirname(vim.fs.find({ ".gradlew", ".git", "mvnw" })[1])
+        cmd = {"/home/ruben-epic/.local/share/nvim/mason/bin/jdtls"},
+        root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
     }
-
-    require("jdtls").start_or_attach(config)
+    require('jdtls').start_or_attach(config)
   end
-
 }
