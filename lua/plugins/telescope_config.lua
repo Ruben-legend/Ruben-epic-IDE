80db457332673454068c1a2f543b15e3f2fc424f
Ruben-epic <rubenalexander644@outloook.es>
2023-11-20 15:26:22 -0400

    Fix: Telescope
    Fixing Telescope keymaps

--- lua/plugins/telescope_config.lua
+++ lua/plugins/telescope_config.lua
@@ -1,88 +1,133 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  dependencies = {
-    'nvim-telescope/telescope-file-browser.nvim',
-    'nvim-lua/plenary.nvim',
-  },
-  config = function()
-    local telescope = require("telescope")
-    local actions = require('telescope.actions')
-    local builtin = require("telescope.builtin")
+	"nvim-telescope/telescope.nvim",
+	lazy = false,
+	keys = {
+		{
+			"<leader><space>",
+			function()
+				local builtin = require("telescope.builtin")
+				builtin.find_files({
+					hidden = true,
+					file_ignore_patterns = {
+						".git/",
+						"^node_modules/",
+						".gradle/",
+						"gradle/",
+						"app/build/",
+						"app/bin",
+						"app/.classpath",
+						"app/.proyect",
+						"app/.setting",
+						".build/",
+					},
+				})
+			end,
+			desc = "Find files",
+		},
+		{
+			";f",
+			function()
+				local telescope = require("telescope")
+				local function telescope_buffer_dir()
+					return vim.fn.expand("%:p:h")
+				end
+				telescope.extensions.file_browser.file_browser({
+					path = "%:p:h",
+					cwd = telescope_buffer_dir(),
+					respect_gitignore = false,
+					hidden = true,
+					grouped = true,
+					previewer = false,
+					initial_mode = "normal",
+					layout_config = { height = 40 },
+				})
+			end,
+			desc = "File Browser",
+		},
+		{
+			";e",
+			function()
+				local builtin = require("telescope.builtin")
+				builtin.diagnostics()
+			end,
+			desc = "Diagnostics",
+		},
+		{
+			";;",
+			function()
+				local builtin = require("telescope.builtin")
+				builtin.resume()
+			end,
+			desc = "Resume telescope",
+		},
+		{
+			";t",
+			function()
+				local builtin = require("telescope.builtin")
+				builtin.help_tags()
+			end,
+			desc = "Help Tags",
+		},
+		{
+			"\\\\",
+			function()
+				local builtin = require("telescope.builtin")
+				builtin.buffers()
+			end,
+			desc = "Show buffers",
+		},
+		{
+			";r",
+			function()
+				local builtin = require("telescope.builtin")
+				builtin.live_grep()
+			end,
+			desc = "Rip grep",
+		},
+	},
+	dependencies = {
+		"nvim-telescope/telescope-file-browser.nvim",
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
 
-    local function telescope_buffer_dir()
-      return vim.fn.expand('%:p:h')
-    end
+		local fb_actions = require("telescope").extensions.file_browser.actions
 
-    local fb_actions = require "telescope".extensions.file_browser.actions
+		telescope.setup({
+			defaults = {
+				mappings = {
+					n = {
+						["q"] = actions.close,
+					},
+				},
+			},
+			extensions = {
+				file_browser = {
+					theme = "dropdown",
+					-- disables netrw and use telescope-file-browser in its place
+					hijack_netrw = true,
+					mappings = {
+						-- your custom insert mode mappings
+						["i"] = {
+							["<C-w>"] = function()
+								vim.cmd("normal vbd")
+							end,
+						},
+						["n"] = {
+							-- your custom normal mode mappings
+							["N"] = fb_actions.create,
+							["h"] = fb_actions.goto_parent_dir,
+							["/"] = function()
+								vim.cmd("startinsert")
+							end,
+						},
+					},
+				},
+			},
+		})
 
-    telescope.setup {
-      defaults = {
-        mappings = {
-          n = {
-            ["q"] = actions.close
-          },
-        },
-      },
-      extensions = {
-        file_browser = {
-          theme = "dropdown",
-          -- disables netrw and use telescope-file-browser in its place
-          hijack_netrw = true,
-          mappings = {
-            -- your custom insert mode mappings
-            ["i"] = {
-              ["<C-w>"] = function() vim.cmd('normal vbd') end,
-            },
-            ["n"] = {
-              -- your custom normal mode mappings
-              ["N"] = fb_actions.create,
-              ["h"] = fb_actions.goto_parent_dir,
-              ["/"] = function()
-                vim.cmd('startinsert')
-              end
-            },
-          },
-        },
-      },
-    }
-
-    telescope.load_extension("file_browser")
-
-    vim.keymap.set('n', ';f',
-      function()
-        builtin.find_files({
-          no_ignore = false,
-          hidden = true,
-          file_ignore_patterns = { ".git/", "^node_modules/", ".gradle/", "gradle/", "app/build/", "app/bin", "app/.classpath", "app/.proyect", "app/.setting", ".build/"}
-        })
-      end)
-    vim.keymap.set('n', ';r', function()
-      builtin.live_grep()
-    end)
-    vim.keymap.set('n', '\\\\', function()
-      builtin.buffers()
-    end)
-    vim.keymap.set('n', ';t', function()
-      builtin.help_tags()
-    end)
-    vim.keymap.set('n', ';;', function()
-      builtin.resume()
-    end)
-    vim.keymap.set('n', ';e', function()
-      builtin.diagnostics()
-    end)
-    vim.keymap.set("n", "sf", function()
-      telescope.extensions.file_browser.file_browser({
-        path = "%:p:h",
-        cwd = telescope_buffer_dir(),
-        respect_gitignore = false,
-        hidden = true,
-        grouped = true,
-        previewer = false,
-        initial_mode = "normal",
-        layout_config = { height = 40 }
-      })
-    end)
-  end
-
+		telescope.load_extension("file_browser")
+	end,
 }
