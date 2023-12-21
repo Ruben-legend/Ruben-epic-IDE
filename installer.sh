9e7e77efc7e40d12a7a6e625c0cdd88dfbd0baf6
Ruben-epic <rubenalexander644@outlook.es>
2023-12-21 12:39:10 -0400

    test: installer 2

--- installer.sh
+++ installer.sh
@@ -53,13 +53,11 @@ InstallDependencies() {
 	if ! command -v sdk; then
 		curl -s "https://get.sdkman.io" | bash
 		. ~/.sdkman/bin/sdkman-init.sh
-		zsh
 		sdk version
 	fi
 	if ! command -v java; then
 		sdk install java
 		sdk install gradle
-		exit
 	fi
 }
 
